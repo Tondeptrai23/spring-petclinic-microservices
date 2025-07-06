d5e294c39b04bb643e44bc74b0ea3777cadf82cd
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2025-07-06 08:00:05 +0700

    test dev bump

--- spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/web/VetResourceTest.java
+++ spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/web/VetResourceTest.java
@@ -42,7 +42,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class VetResourceTest {
 
     @Autowired
-    MockMvc mvc; // test 1
+    MockMvc mvc; // test 2
 
     @MockBean
     VetRepository vetRepository;
